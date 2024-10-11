43d404de9df1424b990625559522e8374c64d78c
trimerictube <jeanricardo.jr123@gmail.com>
2024-10-11 09:03:56 -0300

    11.10 - lista organizacional

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercicios PHP</title>
+</head>
+<body>
+<?php
+        echo "<h1>Exercicio_PHP1: </h1>";
+        echo "<a href='/exercicios/exercico_php1/1.php'>1)</a><br>";
+        echo "<a href='/exercicios/exercico_php1/2.php'>2)</a><br>";
+
+        ?>
+</body>
+</html>
